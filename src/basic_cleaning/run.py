33ae1d7f5b649beb1a791ae7cd30e63fc2015a56
Manuel Carmona <manuelvillarcarmona@gmail.com>
2024-04-08 19:15:28 +0200

    Add step 5.2: fix basic_cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,6 +28,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Dropping values outside boundaries")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving cleaned df and uploading to wandb")
     df.to_csv("clean_sample.csv", index=False)
 
